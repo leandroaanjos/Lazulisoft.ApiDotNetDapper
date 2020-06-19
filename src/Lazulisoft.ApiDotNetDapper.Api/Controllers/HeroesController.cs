bc92b294ea8a9d23b26be9d76912af9ca4a9223c
Leandro Dos Anjos <laamaker@gmail.com>
2020-06-19 18:43:47 -0300

    Added heroes controller

---
+++ src/Lazulisoft.ApiDotNetDapper.Api/Controllers/HeroesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Lazulisoft.ApiDotNetDapper.Api.Models;
+using Lazulisoft.ApiDotNetDapper.Api.Repositories;
+using Lazulisoft.ApiDotNetDapper.Api.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lazulisoft.ApiDotNetDapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroesController : ControllerBase
+    {
+        private readonly IHeroRepository _heroRepository;
+        private readonly IMapper _mapper;
+
+        public HeroesController(IHeroRepository heroRepository, IMapper mapper)
+        {
+            _heroRepository = heroRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var heroes = await _heroRepository.GetAll();
+            return Ok(_mapper.Map<List<HeroViewModel>>(heroes));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var hero = await _heroRepository.Get(id);
+
+            if (hero == null)
+                return BadRequest();
+
+            return Ok(_mapper.Map<HeroViewModel>(hero));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] HeroViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var newHero = _mapper.Map<Hero>(model);
+                await _heroRepository.Add(newHero);
+
+                return CreatedAtAction(nameof(Get), new { id = newHero.Id }, _mapper.Map<HeroViewModel>(newHero));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Msg = ex.Message });
+            }
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> Put([FromBody] HeroViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var updateHero = await _heroRepository.Get(model.Id.Value);
+
+                if (updateHero == null)
+                    return NotFound();
+
+                updateHero = _mapper.Map<Hero>(model);
+                await _heroRepository.Update(updateHero);
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Msg = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var deleteHero = await _heroRepository.Get(id);
+
+                if (deleteHero == null)
+                    return NotFound();
+
+                await _heroRepository.Delete(deleteHero.Id);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Msg = ex.Message
+                });
+            }
+        }
+    }
+}
---
+++ src/Lazulisoft.ApiDotNetDapper.Api/ViewModels/HeroViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Lazulisoft.ApiDotNetDapper.Api.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lazulisoft.ApiDotNetDapper.Api.ViewModels
+{
+    public class HeroViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public Publisher Publisher { get; set; }
+
+        [MaxLength(50)]
+        public string AlterEgo { get; set; }
+
+        public bool HasSuperPower { get; set; }
+
+        [MaxLength(200)]
+        public string Abilities { get; set; }
+    }
+}
